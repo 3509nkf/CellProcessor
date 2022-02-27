0de390f6c0da0ef9aca936d594f11eb1a131c3b4
3509nkf <nagokaitofabian@gmail.com>
2022-02-27 09:53:19 +0900

    AddOperator.java

---
+++ Employee.java
@@ -0,0 +1,38 @@
+package sample;
+import java.util.Date;
+
+
+public class Employee {
+	private String name;
+	private Integer age;
+	private Date birth;
+	private String email;
+	private String note;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+	
+	
+	
+	
+	
+
+}
